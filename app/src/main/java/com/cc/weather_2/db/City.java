a93dcdc69e6182beef2189854f2a2fd3210f6f40
Tony <tony@gmail.com>
2018-11-02 14:31:07 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/cc/weather_2/db/City.java
@@ -0,0 +1,36 @@
+package com.cc.weather_2.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class City extends LitePalSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int provinceId;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getCityName(){
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
